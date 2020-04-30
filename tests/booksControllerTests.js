39b3a90041ae468f51cc6bebfef9468769710b48
Caden Chabot <chabotc@myumanitoba.ca>
2020-04-30 10:00:24 -0500

    initial commit

---
+++ tests/booksControllerTests.js
@@ -0,0 +1,28 @@
+const should = require('should');
+const sinon = require('sinon');
+const booksController = require('../controllers/booksController');
+
+describe('Book Controller', () => {
+  describe('Post', () => {
+    it('should not allow an empty title', () => {});
+    const Book = function (book) {
+      this.save = () => {};
+    };
+    const req = {
+      body: {
+        author: "John"
+      }
+    };
+
+    const res = {
+      status: sinon.spy(),
+      send: sinon.spy(),
+      json: sinon.spy()
+    };
+
+    const controller = booksController(Book);
+    controller.post(req, res);
+    res.status.calledWith(400).should.equal(true, `Bad status ${res.status.args[0][0]}`);
+    res.send.calledWith('Title is required').should.equal(true);
+  });
+});
